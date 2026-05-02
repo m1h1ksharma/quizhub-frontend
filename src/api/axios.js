17eea7544a4892bd8951a9dbdb424715a97ea0d3
Mahak <mahak3946@gmail.com>
2026-05-02 08:45:06 +0530

    added

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,23 +1,29 @@
 import axios from "axios";
 
-// Ekdum saaf URL bina kisi extra slash ke
+// Backend Base URL
 const base = "https://quizhub-backend-fesf.onrender.com/api";
 
 const API = axios.create({
-  baseURL: base, 
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: base,
 });
 
 // Request Interceptor
 API.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-    // Debugging ke liye: console mein check kar sakte ho ki request kahan ja rahi hai
+
+    // IMPORTANT:
+    // Agar FormData hai toh browser khud multipart set karega
+    if (config.data instanceof FormData) {
+      delete config.headers["Content-Type"];
+    } else {
+      config.headers["Content-Type"] = "application/json";
+    }
+
     console.log("Request URL:", config.baseURL + config.url);
     return config;
   },
